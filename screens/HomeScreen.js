ab70a5c711fccf47a661ee659f32aa8a80886a23
jeremy_woo <jeremy.woo@clearpoint.co.nz>
2018-08-01 22:47:40 +1200

    added icon for image picker and camera

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -7,167 +7,177 @@ import { ActivityIndicator, Button, Clipboard, Image,
   Text, TextInput,
   TouchableOpacity,
   View } from 'react-native';
-import { Constants, ImagePicker, Permissions, Alert } from 'expo';
-export default class HomeScreen extends React.Component {
+  import { Constants, ImagePicker, Permissions, Alert } from 'expo';
+  import { Icon } from 'react-native-elements'
   
   
-  static navigationOptions = {
-    header: null,
-  };
-  
-  state = {
-    image: null,
-    uploading: false,
-  };
-
-
-  _pickImage = async () => {
-    const {
-      status: cameraRollPerm
-    } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
-
-    // only if user allows permission to camera roll
-    if (cameraRollPerm === 'granted') {
-      let pickerResult = await ImagePicker.launchImageLibraryAsync({
-        allowsEditing: false,
-        //aspect: [4, 3],
-      });
-
-      this._handleImagePicked(pickerResult);
-    }
-  };
- 
-  async componentWillMount() {
-    const { status } = await Permissions.askAsync(Permissions.CAMERA);
-    this.setState({ hasCameraPermission: status === 'granted' });
-  }
-  
-  takePicture = () => {    
-    if (this.camera) {
-      this.camera.takePictureAsync({ onPictureSaved: this.onPictureSaved });
-    }
-  };
-  
-  onPictureSaved = async photo => {
-    await FileSystem.moveAsync({
-      from: photo.uri,
-      to: `${FileSystem.documentDirectory}photos/${Date.now()}.jpg`,
-    });
-    this.setState({ newPhotos: true });
-  }
-
-  render() {
-    let {
-      image
-    } = this.state;
-
-    return (
-      <View style={styles.tabBarInfoContainer}>           
+  export default class HomeScreen extends React.Component {
+    
+    
+    static navigationOptions = {
+      header: null,
+    };
+    
+    state = {
+      image: null,
+      uploading: false,
+    };
+        
+    _pickImage = async () => {
+      const {
+        status: cameraRollPerm
+      } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
       
-        <StatusBar barStyle="default" />
-
-        <Button
-          onPress={this._pickImage}
-          title="Pick an image"
-        />
-
-        <Button onPress={this._takePhoto} title="Take a photo" />
-
-        {this._maybeRenderImage()}
-        {this._maybeRenderUploadingOverlay()}
-
-      </View>
-    );
-  }
-
-  _takePhoto = async () => {
-
-    await Alert.alert(
-      'Alert Title',
-      'My Alert Msg',
-      [
-        {text: 'Ask me later', onPress: () => console.log('Ask me later pressed')},
-        {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
-        {text: 'OK', onPress: () => console.log('OK Pressed')},
-      ],
-      { cancelable: false }
-    )
-
-
-    const {
-      status: cameraPerm
-    } = await Permissions.askAsync(Permissions.CAMERA);
-
-    const {
-      status: cameraRollPerm
-    } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
-
-    // only if user allows permission to camera AND camera roll
-    if (cameraPerm === 'granted' && cameraRollPerm === 'granted') {
-      let pickerResult = await ImagePicker.launchCameraAsync({
-        //allowsEditing: true,
-        //aspect: [4, 3],
-      });
-
-      
-      this._handleImagePicked(pickerResult);
+      // only if user allows permission to camera roll
+      if (cameraRollPerm === 'granted') {
+        let pickerResult = await ImagePicker.launchImageLibraryAsync({
+          allowsEditing: false,
+          //aspect: [4, 3],
+        });
+        
+        this._handleImagePicked(pickerResult);
+      }
+    };
+    
+    async componentWillMount() {
+      const { status } = await Permissions.askAsync(Permissions.CAMERA);
+      this.setState({ hasCameraPermission: status === 'granted' });
     }
-  };
-  
-  _maybeRenderImage = () => {
+    
+    takePicture = () => {    
+      if (this.camera) {
+        this.camera.takePictureAsync({ onPictureSaved: this.onPictureSaved });
+      }
+    };
+    
+    onPictureSaved = async photo => {
+      await FileSystem.moveAsync({
+        from: photo.uri,
+        to: `${FileSystem.documentDirectory}photos/${Date.now()}.jpg`,
+      });
+      this.setState({ newPhotos: true });
+    }
+    
+    render() {
       let {
         image
       } = this.state;
-  
+      
+      return (
+        
+        <View style={styles.tabBarInfoContainer}>           
+        
+        
+        <Icon
+        raised
+        name='image'
+        type='font-awesome'
+        color='#f50'
+        onPress={this._pickImage} />
+        
+        <Icon
+        raised
+        name='camera'
+        type='font-awesome'
+        color='#f50'
+        onPress={this._takePhoto} />
+        
+        
+        {this._maybeRenderImage()}
+        {this._maybeRenderUploadingOverlay()}
+        
+        </View>
+      );
+    }
+    
+    _takePhoto = async () => {
+      
+      await Alert.alert(
+        'Alert Title',
+        'My Alert Msg',
+        [
+          {text: 'Ask me later', onPress: () => console.log('Ask me later pressed')},
+          {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+          {text: 'OK', onPress: () => console.log('OK Pressed')},
+        ],
+        { cancelable: false }
+      )
+      
+      
+      const {
+        status: cameraPerm
+      } = await Permissions.askAsync(Permissions.CAMERA);
+      
+      const {
+        status: cameraRollPerm
+      } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+      
+      // only if user allows permission to camera AND camera roll
+      if (cameraPerm === 'granted' && cameraRollPerm === 'granted') {
+        let pickerResult = await ImagePicker.launchCameraAsync({
+          //allowsEditing: true,
+          //aspect: [4, 3],
+        });
+        
+        
+        this._handleImagePicked(pickerResult);
+      }
+    };
+    
+    _maybeRenderImage = () => {
+      let {
+        image
+      } = this.state;
+      
       if (!image) {
         return;
       }
-  
+      
       return (
         <View
-          style={styles.maybeRenderContainer}>
-          <View
-            style={styles.maybeRenderImageContainer}>
-            <Image source={{ uri: image }} style={styles.maybeRenderImage} />
-          </View>
-  
-          <Text
-            onPress={this._copyToClipboard}
-            onLongPress={this._share}
-            style={styles.maybeRenderImageText}>
-            {image}
-          </Text>
+        style={styles.maybeRenderContainer}>
+        <View
+        style={styles.maybeRenderImageContainer}>
+        <Image source={{ uri: image }} style={styles.maybeRenderImage} />
+        </View>
+        
+        <Text
+        onPress={this._copyToClipboard}
+        onLongPress={this._share}
+        style={styles.maybeRenderImageText}>
+        {image}
+        </Text>
         </View>
       );
     };
-
+    
     _maybeRenderUploadingOverlay = () => {
       if (this.state.uploading) {
         return (
           <View
-            style={[StyleSheet.absoluteFill, styles.maybeRenderUploading]}>
-            <ActivityIndicator color="#fff" size="large" />
+          style={[StyleSheet.absoluteFill, styles.maybeRenderUploading]}>
+          <ActivityIndicator color="#fff" size="large" />
           </View>
         );
       }
     };    
-
-
-    _handleImagePicked = async pickerResult => {
     
-      let uploadResponse, uploadResult;
-
+    
+    _handleImagePicked = async pickerResult => {
       
-  
+      let uploadResponse, uploadResult;
+      
+      
+      
       try {
         this.setState({
           uploading: true
         });
-  
+        
         if (!pickerResult.cancelled) {
           uploadResponse = await uploadImageAsync(pickerResult.uri);
           uploadResult = await uploadResponse.json();
-  
+          
           this.setState({
             image: uploadResult.location
           });
@@ -183,10 +193,10 @@ export default class HomeScreen extends React.Component {
         });
       }
     };
-  
+    
     async uploadImageAsync(uri) {
       let apiUrl = 'https://file-upload-example-backend-dkhqoilqqn.now.sh/upload';
-    
+      
       // Note:
       // Uncomment this if you want to experiment with local server
       //
@@ -195,17 +205,17 @@ export default class HomeScreen extends React.Component {
       // } else {
       //   apiUrl = `http://localhost:3000/upload`
       // }
-    
+      
       let uriParts = uri.split('.');
       let fileType = uriParts[uriParts.length - 1];
-    
+      
       let formData = new FormData();
       formData.append('photo', {
         uri,
         name: `photo.${fileType}`,
         type: `image/${fileType}`,
       });
-    
+      
       let options = {
         method: 'POST',
         body: formData,
@@ -214,76 +224,76 @@ export default class HomeScreen extends React.Component {
           'Content-Type': 'multipart/form-data',
         },
       };
-    
+      
       return fetch(apiUrl, options);
     }
- 
-  }   
     
-const styles = StyleSheet.create({
-  tabBarInfoContainer: {
-    flex : 1, 
-    flexDirection : 'row',
-    justifyContent : 'space-evenly',
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'stretch',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  container: {
-    alignItems: 'center',
-    flex: 1,
-    justifyContent: 'center',
-  },
-  exampleText: {
-    fontSize: 20,
-    marginBottom: 20,
-    marginHorizontal: 15,
-    textAlign: 'center',
-  },
-  maybeRenderUploading: {
-    alignItems: 'center',
-    backgroundColor: 'rgba(0,0,0,0.4)',
-    justifyContent: 'center',
-  },
-  maybeRenderContainer: {
-    borderRadius: 3,
-    elevation: 2,
-    marginTop: 30,
-    shadowColor: 'rgba(0,0,0,1)',
-    shadowOpacity: 0.2,
-    shadowOffset: {
-      height: 4,
-      width: 4,
+  }   
+  
+  const styles = StyleSheet.create({
+    tabBarInfoContainer: {
+      flex : 1, 
+      flexDirection : 'row',
+      justifyContent : 'space-evenly',
+      position: 'absolute',
+      bottom: 0,
+      left: 0,
+      right: 0,
+      ...Platform.select({
+        ios: {
+          shadowColor: 'black',
+          shadowOffset: { height: -3 },
+          shadowOpacity: 0.1,
+          shadowRadius: 3,
+        },
+        android: {
+          elevation: 20,
+        },
+      }),
+      alignItems: 'stretch',
+      backgroundColor: '#fbfbfb',
+      paddingVertical: 20,
     },
-    shadowRadius: 5,
-    width: 250,
-  },
-  maybeRenderImageContainer: {
-    borderTopLeftRadius: 3,
-    borderTopRightRadius: 3,
-    overflow: 'hidden',
-  },
-  maybeRenderImage: {
-    height: 250,
-    width: 250,
-  },
-  maybeRenderImageText: {
-    paddingHorizontal: 10,
-    paddingVertical: 10,
-  }
-});
+    container: {
+      alignItems: 'center',
+      flex: 1,
+      justifyContent: 'center',
+    },
+    exampleText: {
+      fontSize: 20,
+      marginBottom: 20,
+      marginHorizontal: 15,
+      textAlign: 'center',
+    },
+    maybeRenderUploading: {
+      alignItems: 'center',
+      backgroundColor: 'rgba(0,0,0,0.4)',
+      justifyContent: 'center',
+    },
+    maybeRenderContainer: {
+      borderRadius: 3,
+      elevation: 2,
+      marginTop: 30,
+      shadowColor: 'rgba(0,0,0,1)',
+      shadowOpacity: 0.2,
+      shadowOffset: {
+        height: 4,
+        width: 4,
+      },
+      shadowRadius: 5,
+      width: 250,
+    },
+    maybeRenderImageContainer: {
+      borderTopLeftRadius: 3,
+      borderTopRightRadius: 3,
+      overflow: 'hidden',
+    },
+    maybeRenderImage: {
+      height: 250,
+      width: 250,
+    },
+    maybeRenderImageText: {
+      paddingHorizontal: 10,
+      paddingVertical: 10,
+    }
+  });
