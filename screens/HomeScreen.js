ab4897cd3549937defe5851fadfb2d15185ec137
jeremy_woo <jeremy.woo@clearpoint.co.nz>
2018-08-01 22:50:23 +1200

    removed alert from code.

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -89,21 +89,9 @@ import { ActivityIndicator, Button, Clipboard, Image,
         </View>
       );
     }
-    
+        
     _takePhoto = async () => {
-      
-      await Alert.alert(
-        'Alert Title',
-        'My Alert Msg',
-        [
-          {text: 'Ask me later', onPress: () => console.log('Ask me later pressed')},
-          {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
-          {text: 'OK', onPress: () => console.log('OK Pressed')},
-        ],
-        { cancelable: false }
-      )
-      
-      
+
       const {
         status: cameraPerm
       } = await Permissions.askAsync(Permissions.CAMERA);
